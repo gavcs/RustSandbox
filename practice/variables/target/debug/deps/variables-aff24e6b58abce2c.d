80f8816648586df5bc49d7979c9c6fcb79a2bc44
Gavin McConnell <gavinwmcconnell@gmail.com>
2024-05-08 19:29:52 -0400

    finished 3.1

---
+++ practice/variables/target/debug/deps/variables-aff24e6b58abce2c.d
@@ -0,0 +1,5 @@
+/home/frog/Downloads/RustSandbox/practice/variables/target/debug/deps/variables-aff24e6b58abce2c: src/main.rs
+
+/home/frog/Downloads/RustSandbox/practice/variables/target/debug/deps/variables-aff24e6b58abce2c.d: src/main.rs
+
+src/main.rs:
---
+++ practice/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/frog/Downloads/RustSandbox/practice/variables/target/debug/variables: /home/frog/Downloads/RustSandbox/practice/variables/src/main.rs
