1b3e0bc6ad4230d52fab03d397c693158f83d83a
Gavin <frog@localhost.localdomain>
2024-05-08 19:14:15 -0400

    finished 1.3

---
+++ practice/hello_cargo/target/debug/deps/hello_cargo-c0fac21f3e41d08d.d
@@ -0,0 +1,5 @@
+/home/frog/Downloads/RustSandbox/practice/hello_cargo/target/debug/deps/libhello_cargo-c0fac21f3e41d08d.rmeta: src/main.rs
+
+/home/frog/Downloads/RustSandbox/practice/hello_cargo/target/debug/deps/hello_cargo-c0fac21f3e41d08d.d: src/main.rs
+
+src/main.rs:
---
+++ practice/hello_cargo/target/debug/deps/hello_cargo-f480727d51eef5fb.d
@@ -0,0 +1,5 @@
+/home/frog/Downloads/RustSandbox/practice/hello_cargo/target/debug/deps/hello_cargo-f480727d51eef5fb: src/main.rs
+
+/home/frog/Downloads/RustSandbox/practice/hello_cargo/target/debug/deps/hello_cargo-f480727d51eef5fb.d: src/main.rs
+
+src/main.rs:
