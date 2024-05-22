dbffb02d96b0ea16e895bcf2b2b4a88931efb399
gavcs <gavinwmcconnell@gmail.com>
2024-05-21 18:55:40 -0700

    finished guessing game

--- project/guessing_game/src/main.rs
+++ project/guessing_game/src/main.rs
@@ -1,24 +1,66 @@
-use std::io;
-use std::cmp::Ordering;
+// use std::io;
+// use std::cmp::Ordering;
+// use rand::Rng;
+
+// fn main() {
+//     println!("Guess the number!");
+    
+//     let mut guess = String::new();
+    
+//     let target = rand::thread_rng().gen_range(1..=100);
+//     println!("The correct number is {target}.");
+//     loop {
+//         print!("Please input your guess: ");
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("Failed to read line");
+
+//         let guess: u32 = guess.trim().parse().expect("Guess is not a number.");
+        
+//         println!("You guessed: {guess}");
+//         match guess.cmp(&target) {
+//             Ordering::Less => println!("Too Low"),
+//             Ordering::Greater => println!("Too High"),
+//             Ordering::Equal => println!("Correct!"),
+//         }
+//         let mut guess: String = String::new();
+//     }
+// }
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
-    print!("Please input your guess: ");
-    let mut guess = String::new();
-    
-    let target = rand::thread_rng().gen_range(1..=100);
-    io::Write::flush(&mut io::stdout()).expect("Write failed");
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
 
-    let guess: usize = guess.trim().parse().expect("Guess is not a number.");
-    println!("The correct number is {target}.");
-    println!("You guessed: {guess}");
-    match guess.cmp(&target) {
-        Ordering::Less => println!("Too Low"),
-        Ordering::Greater => println!("Too High"),
-        Ordering::Equal => println!("Correct!"),
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        print!("Please input your guess: ");
+
+        io::Write::flush(&mut io::stdout()).expect("Unable to flush.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
