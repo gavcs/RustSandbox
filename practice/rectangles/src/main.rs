4e64ea167ef6e795588054c6fd7d3b89fa8e2567
gavcs <gavinwmcconnell@gmail.com>
2024-06-14 15:07:19 -0700

    currently on 6.1

--- practice/rectangles/src/main.rs
+++ practice/rectangles/src/main.rs
@@ -1,22 +1,68 @@
 #[derive(Debug)]
 struct Rectangle {
-    length: u32,
+    height: u32,
     width: u32,
 }
 
-fn main() {
-    let scale = 2;
+impl Rectangle {
 
-    let r1 = Rectangle {
-        length: dbg!(25 * scale),
-        width: 40,
-    };
-    let a = area(&r1);
-    println!("r1 = {:#?}\n", r1);
-    println!("The area is {a}\n");
-    dbg!(&r1);
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        println!("\nself has height {} and width {}.", self.height, self.width);
+        println!("other has height {} and width {}.\n", other.height, other.width);
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
 }
 
-fn area(r: &Rectangle) -> u32 {
-    r.width * r.length
+fn main() {
+
+    let r1 = Rectangle {
+        height: 50,
+        width: 30,
+    };
+    let r2 = Rectangle {
+        height: 40,
+        width: 10,
+    };
+    let r3 = Rectangle {
+        height: 30,
+        width: 15,
+    };
+
+    println!(
+        "The area of rectangle 1 with width of {} and height of {} is {}.",
+        r1.width,
+        r1.height,
+        r1.area()
+    );
+    println!(
+        "The area of rectangle 2 with width of {} and height of {} is {}.",
+        r2.width,
+        r2.height,
+        r2.area(),
+    );
+    println!(
+        "The area of rectangle 3 with width of {} and height of {} is {}.",
+        r3.width,
+        r3.height,
+        r3.area(),
+    );
+
+    println!("Can rectangle 1 hold rectangle 2? {}\n", r1.can_hold(&r2));
+    println!("Can rectangle 2 hold rectangle 3? {}\n", r2.can_hold(&r3));
+
+    print!("Making a square with side size of 5: ");
+    let sq = Rectangle::square(5);
+    println!("side 1 = {}, side 2 = {}", sq.width, sq.height);
 }
