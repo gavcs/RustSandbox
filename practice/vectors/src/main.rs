7ce9e397ce6940458ea940855767c81baf4078db
gavcs <gavinwmcconnell@gmail.com>
2024-07-12 15:53:45 -0400

    completed unit 8

--- practice/vectors/src/main.rs
+++ practice/vectors/src/main.rs
@@ -1,4 +1,41 @@
 fn main() {
+    median();
+}
+
+fn median() {
+    let mut vec = vec![23, 67, 38, 11, 94, 13];
+    vec.sort();
+    for elem in &vec {
+        println!("{elem}");
+    }
+    let size = vec.len();
+    match size%2{
+        0 => {
+            let l = size/2;
+            let n1 = vec.get(l);
+            let n2 = vec.get(l-1);
+            if let Some(num1) = n1 {
+                println!("num1 = {num1}");
+                if let Some(num2) = n2 {
+                    println!("num2 = {num2}");
+                    let add = (num1 + num2);
+                    let median = add as f32;
+                    let median = median/2.0;
+                    println!("The middle is {median}.");
+                }
+            }            
+        },
+        _ => {
+            let l = (size + 1)/2;
+
+            if let Some(num) = vec.get(l) {
+                println!("The middle is {num}");
+            };
+        },
+    };
+}
+
+fn stuff() {
     let mut v: Vec<i32> = Vec::new();   // or let v = vec![1, 2, 3]; to initialize with values
 
     // add values to the vector
