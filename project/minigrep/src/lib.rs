ca046f1c09301d8bb2a2999ece6eb412d6b2af67
gavcs <gavinwmcconnell@gmail.com>
2024-07-18 11:13:06 -0400

    finished minigrep project

--- project/minigrep/src/lib.rs
+++ project/minigrep/src/lib.rs
@@ -1,10 +1,12 @@
 
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     substring: String,
     filename: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -14,23 +16,50 @@ impl Config {
         }
         let substring = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config{substring, filename})
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config{substring, filename, ignore_case})
     }
 
-    pub fn new(substring: String, filename: String) -> Config {
-        Config{substring, filename}
+    pub fn new(substring: String, filename: String, ignore_case: bool) -> Config {
+        Config{substring, filename, ignore_case}
     }
 }
 
 pub fn run(cfg: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(cfg.filename)?;
-    println!("{}\n{contents}", cfg.substring);
+    
+    let results = if cfg.ignore_case {
+        search_case_insensitive(&cfg.substring, &contents)
+    } else {
+        search(&cfg.substring, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
 
     Ok(())
 }
 
 pub fn search<'a>(substring: &str, contents: &'a str) -> Vec<&'a str> { 
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(substring) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(substring: &str, contents: &'a str) -> Vec<&'a str> {
+    let substring = substring.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&substring) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -41,27 +70,24 @@ mod test {
     fn newtest() {
         let substring = String::from("hello");
         let filename = String::from("five");
-        let actual = Config::new(substring.clone(), filename.clone());
-        let expected = Config{substring, filename};
+        let ignore_case = true;
+        let actual = Config::new(substring.clone(), filename.clone(), ignore_case);
+        let expected = Config{substring, filename, ignore_case};
         assert_eq!(actual.filename, expected.filename);
         assert_eq!(actual.substring, expected.substring);
     }
 
     #[test]
-    fn runtest() {
-        let substring = String::from("name");
-        let filename = String::from("poem");
-        let con = Config{substring, filename};
-        let actual = run(con).unwrap();
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nDuct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-                            Rust:
-                            safe, fast, productive.
-                            Pick three.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "Rust:\nsafe, fast, productive.\nPick three.\nTrust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 }
