5fd912ce112135d78c77bf6711b00fdbc3ff097a
gavcs <gavinwmcconnell@gmail.com>
2024-08-19 14:06:39 -0400

    15.5

--- practice/boxes/src/main.rs
+++ practice/boxes/src/main.rs
@@ -1,10 +1,23 @@
+#[derive(Debug)]
 enum List {
-    Cons(i32, Box<List>),
+    Cons(Rc<RefCell<i32>>, Rc<List>),
     Nil,
 }
 
 use List::{Cons, Nil};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 fn main() {
-    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
---
+++ practice/maxval/target/debug/deps/maxval-5d273acbf71c22d3.d
@@ -0,0 +1,5 @@
+/Users/frog/Downloads/RustSandbox/practice/maxval/target/debug/deps/maxval-5d273acbf71c22d3: src/main.rs
+
+/Users/frog/Downloads/RustSandbox/practice/maxval/target/debug/deps/maxval-5d273acbf71c22d3.d: src/main.rs
+
+src/main.rs:
